8065bde89848ab1e2c2926cadfb4b16e00d9473e
RavinduPonnamperuma <ravindu.ponnamperuma@gmail.com>
2023-05-09 16:57:34 +0530

    intialized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
